c5732ebaea85c9c27ae67d5087f8319649cd62f5
LeonFassbinder <118434026+LeonFassbinder@users.noreply.github.com>
2024-10-08 08:21:33 +0200

    feat: Mitgliederverwaltung
    Mitglieder können gelöscht, oder einzelne Einträge geändert werden.

--- Frontend/app/Controllers/MitgliederVerwalten.php
+++ Frontend/app/Controllers/MitgliederVerwalten.php
@@ -4,11 +4,264 @@ namespace App\Controllers;
 
 class MitgliederVerwalten extends BaseController
 {
+	
+	
     public function index()
     {
+		$viewData['users'] = $this->getUser();
+		$viewData['addresses'] = $this->getAddress();
+		
+		//print_r($viewData['users']);
+		//print_r($viewData['addresses']);
+		
 		echo view('templates/header');
 		echo view('templates/navigation');
-        echo view('mitglieder_view');
+        echo view('mitglieder_view', $viewData);
 		echo view('templates/footer');
+		
     }
+	
+	public function getUser()
+	{
+		
+		$url = "http://schule.tequu.ovh:52050/v1/member";
+		$headers = ['x-secret: bihekbnrlkar4324bbdejfjm2'];
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$json = curl_exec($ch);
+
+		if (curl_error($ch)) {
+		  echo "curl Fehler: " . curl_error($ch);
+		} else if ($json === "") {
+		  echo "Das JSON ist leer";
+		} else {
+		  // Verarbeite die JSON-Daten (z.B. mit json_decode)
+		  //echo "Erfolgreich Daten erhalten";
+		}
+		
+		$jsonArray = json_decode($json, true);
+				
+		$count = 0;
+		$users = array();
+		foreach ($jsonArray as $element){
+			$user = [
+				"id" =>$element["id"],
+				"fee" => $element["fee"],
+				"name" => $element["name"],
+				"surname" => $element["surname"],
+				"email" => $element["email"],
+				"joinedAt" => $element["joinedAt"],
+				"exitAt" => $element["exitAt"],
+			];
+			array_push($users, $user);
+			
+			$count += 1;
+		}
+		return $users;
+	}
+	
+	function getAddress() {
+		$url = "http://schule.tequu.ovh:52050/v1/member";
+		$headers = ['x-secret: bihekbnrlkar4324bbdejfjm2'];
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$json = curl_exec($ch);
+		
+		$jsonArray = json_decode($json, true);
+		
+		$addresses = array();
+		foreach ($jsonArray as $element){
+			$address = [
+				"user_id" => $element["id"],
+				"id" => $element["address"]["id"],
+				"houseNumber" => $element["address"]["houseNumber"],
+				"street" => $element["address"]["street"],
+				"city" => $element["address"]["city"],
+				"zip" => $element["address"]["zip"],
+			];
+			
+			array_push($addresses, $address);
+			}
+		return $addresses;
+	}
+	
+	public function postUser() {
+		
+		$lastname = $this->request->getVar('lastname');
+		$firstname = $this->request->getVar('firstname');
+		$email = $this->request->getVar('email');
+		$address = $this->request->getVar('address');
+		$fee = $this->request->getVar('fee');
+		
+		$addressArray1 = explode(", ", $address);
+		$addressCity = explode(" ", $addressArray1[0]);
+		$addressHouse = explode(" ", $addressArray1[1]);
+		
+		$addressArray2 = array(
+			//'id' => 0,
+			'houseNumber' => $addressHouse[1],
+			'street' => $addressHouse[0],
+			'city' => $addressCity[1],
+			'zip' => $addressCity[0]
+		);
+		
+		$userJSON = array(
+			//'id' => 0,
+			'fee' => $fee,
+			'name' => $firstname,
+			'surname' => $lastname,
+			'email' => $email,
+			//'joinedAt' => "2024-10-02T11:34:56.274Z",
+			'joinedAt' => date("Y-m-d\TH:i:s.v\Z"),
+			'exitAt' => "",
+			'address' => $addressArray2
+		);
+
+		$myJson = json_encode($userJSON);
+		
+		echo date("Y-m-d\TH:i:s.v\Z");
+		echo $myJson;
+		
+		$url = "http://schule.tequu.ovh:52050/v1/member";
+		$headers = ['x-secret: bihekbnrlkar4324bbdejfjm2',
+					'Content-Type: application/json',
+					'Accept: application/json'];
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $myJson);
+		
+		$response = curl_exec($ch);
+		echo $response;
+		
+		return redirect()->to('/MitgliederVerwalten');
+	}
+	
+	public function deleteUser() {
+		
+		$checkBoxes = $this->request->getVar('deleteCheckBox');
+		
+		print_r($checkBoxes);
+		
+		foreach($checkBoxes as $checkBox) {
+			$deleteURL = "http://schule.tequu.ovh:52050/v1/member/" . $checkBox;
+			$headers = ['x-secret: bihekbnrlkar4324bbdejfjm2'];
+			
+			$delete = curl_init();
+			curl_setopt($delete, CURLOPT_CUSTOMREQUEST, "DELETE");
+			curl_setopt($delete, CURLOPT_URL, $deleteURL);
+			curl_setopt($delete, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($delete, CURLOPT_RETURNTRANSFER, true);
+			
+			$response = curl_exec($delete);
+			echo $response;
+		}
+	}
+	
+	public function putUser() {
+
+		$userID = $this->request->getVar('hiddenID');
+		$name = $this->request->getVar('name');
+		$surname = $this->request->getVar('surname');
+		$email = $this->request->getVar('nameEmail');
+		$address = $this->request->getVar('nameAddress');
+		$fee = $this->request->getVar('nameFee');
+		$joinedAt = $this->request->getVar('joinedAt');
+		$exitAt = $this->request->getVar('exitAt');
+				
+		for($x = 0; count($userID) > $x; $x++){
+			
+			$addressArray1 = explode(", ", $address[$x]);
+			$addressCity = explode(" ", $addressArray1[0]);
+			$addressHouse = explode(" ", $addressArray1[1]);
+
+			$userArray = $this->getUserByID($userID[$x]);
+			$jsonArray = json_decode($userArray, true);
+			$addressID = $jsonArray['address'];
+						
+			$addressArray2 = array(
+				'id' => $addressID['id'],
+				'houseNumber' => $addressHouse[1],
+				'street' => $addressHouse[0],
+				'city' => $addressCity[1],
+				'zip' => $addressCity[0]
+			);
+			
+			if($exitAt[$x] == ""){
+				$exitAt[$x] = null;
+			}
+			$userJSON = array(
+				'id' => (int)$userID[$x],
+				'fee' => (int)$fee[$x],
+				'name' => $name[$x],
+				'surname' => $surname[$x],
+				'email' => $email[$x],
+				'joinedAt' => $joinedAt[$x],
+				'exitAt' => $exitAt[$x],
+				'address' => $addressArray2
+			);
+			
+			$myJson = json_encode($userJSON, JSON_UNESCAPED_UNICODE);
+		
+			print_r($myJson);
+			print_r($userArray);
+			
+			/*
+			if($myJson == $userArray){
+				print_r ("true");
+			} else {
+				print_r("false");
+			}*/
+			
+		}
+		
+	}
+	
+	public function getUserByID($ID){
+		
+		$url = "http://schule.tequu.ovh:52050/v1/member/" . $ID;
+		$headers = ['x-secret: bihekbnrlkar4324bbdejfjm2'];
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$json = curl_exec($ch);
+
+		if (curl_error($ch)) {
+		  echo "curl Fehler: " . curl_error($ch);
+		} else if ($json === "") {
+		  echo "Das JSON ist leer";
+		} else {
+		  // Verarbeite die JSON-Daten (z.B. mit json_decode)
+		  //echo "Erfolgreich Daten erhalten";
+		}
+		
+		$jsonArray = json_decode($json, true);
+		return $json;
+	}
+	
+	function putUserByID($userJSON, $userID) {
+		
+		$putURL = "http://schule.tequu.ovh:52050/v1/member/" . $userID;
+		$headers = ['x-secret: bihekbnrlkar4324bbdejfjm2'];
+		
+		$put = curl_init();
+		curl_setopt($put, CURLOPT_CUSTOMREQUEST, "PUT");
+		curl_setopt($put, CURLOPT_URL, $putURL);
+		curl_setopt($put, CURLOPT_HTTPHEADER, $headers.strlen($userJSON));
+		curl_setopt($put, CURLOPT_RETURNTRANSFER, true);
+		
+		$response = curl_exec($put);
+		echo $response;
+		
+	}
 }
--- Frontend/app/Views/mitglieder_view.php
+++ Frontend/app/Views/mitglieder_view.php
@@ -1,113 +1,140 @@
 
 
 <div class="mainView">
-	<div class="viewEditMember">
-		<button class="memberButton" id="add" onclick="addMemberLine()">Hinzufügen</button>
-		<button class="memberButton" id="cancelMember" onclick="cancel()">Abbrechen</button>
-		<button class="memberButton" id="save" onclick="save()">Speichern</button>
-		<button class="memberButton" id="change" onclick="changeMember()">Bearbeiten</button>
-		<button class="memberButton" id="delete" onclick="deleteMember()">Löschen</button>
-	</div>
-	<div class="viewHeader">
-	Mitgliederverwaltung
-	</div>
-	<div class="viewTable">
-		<table id="members">
-			<tr id="addMemberLine">
-				<th>
-					<div><input type="string" class="addMember"></div>
-				</th>
-				<th>
-					<div><input type="string" class="addMember"></div>
-				</th>
-				<th>
-					<div><input type="string" class="addMember"></div>
-				</th>
-				<th>
-					<div><input type="string" class="addMember"></div>
-				</th>
-				<th>
-					<div><input type="string" class="addMember"></div>
-				</th>
-				<th>
-					<div><input type="string" class="addMember"></div>
-				</th>
-				<th>
-					<div><input type="string" class="addMember"></div>
-				</th>
-			</tr>
-			<tr>
-				<th>Nachname</th>
-				<th>Vorname</th>
-				<th>Sportart</th>
-				<th>Adresse</th>
-				<th>Beitrag</th>
-				<th>Eintritt</th>
-				<th>Austritt</th>
-			</tr>
-			<tbody>
-				<td><input class="changeMember" type="text" value="${row.name}" disabled></td>
-				<td><input class="changeMember" type="text" value="${row.surname}" disabled></td>
-				<td><input class="changeMember" type="text" value="${row.sports}" disabled></td>
-				<td><input class="changeMember" type="text" value="${row.address}" disabled></td>
-				<td><input class="changeMember" type="text" value="${row.fee}" disabled></td>
-				<td><input class="changeMember" type="text" value="${row.joinedAt}" disabled></td>
-				<td><input class="changeMember" type="text" value="${row.exitAt}" disabled></td>
-				<td id="checkBoxBorder"><input id="deleteCheckbox" class="checkBox" type="checkbox"></td>
-			</tbody>
-		</table>
-	</div>
+	<form action="MitgliederVerwalten/postUser" method="post" id="form">
+		<div class="viewEditMember">
+			<button class="memberButton" type="submit" id="addMember">Mitglied Hinzufügen</button>
+			<button class="memberButton" type="button" id="add" onclick="addMemberFunction()">Hinzufügen</button>
+			<button class="memberButton" type="submit" id="save">Speichern</button>
+			<button class="memberButton" type="button" id="change" onclick="changeMemberFunction()">Bearbeiten</button>
+			<button class="memberButton" type="submit" id="deleteMember">Mitglieder Löschen</button>
+			<button class="memberButton" type="button" id="delete" onclick="deleteMemberFunction()">Löschen</button>
+			<button class="memberButton" type="button" id="cancelMember" onclick="cancel()">Abbrechen</button>
+		</div>
+		
+		<div class="viewHeader">
+		Mitgliederverwaltung
+		</div>
+		<div class="viewTable">
+			<table id="members">
+				<tr id="addMemberLine">
+					<th>
+						<div><input type="string" class="addMember" name="lastname"></div>
+					</th>
+					<th>
+						<div><input type="string" class="addMember" name="firstname"></div>
+					</th>
+					<th>
+						<div><input type="string" class="addMember" name="email"></div>
+					</th>
+					<th>
+						<div><input type="string" class="addMember" name="address"></div>
+					</th>
+					<th>
+						<div><input type="string" class="addMember" name="fee"></div>
+					</th>
+				</tr>
+				<tr>
+					<th id="name">Nachname</th>
+					<th id="surname">Vorname</th>
+					<th id="email">E-Mail</th>
+					<th id="address">Adresse</th>
+					<th id="fee">Beitrag</th>
+					<th id="joinedAt">Eintritt</th>
+					<th id="exitAt">Austritt</th>
+				</tr>
+				<?php
+				foreach ($users as $user){
+					$count = 0;
+					$stringAddress = "";
+					
+					if($user['exitAt'] == null) {
+						$exitDate = null;
+					} else {
+						$exitDate = date('Y-m-d', strtotime($user['exitAt']));
+					}
+					
+					foreach ($addresses as $address){
+						
+						if ($address['user_id'] == $user['id']){
+							$stringAddress .= $addresses[$count]['zip'] .= ' ';
+							$stringAddress .= $addresses[$count]['city'] .= ', ';
+							$stringAddress .= $addresses[$count]['street'] .= ' ';
+							$stringAddress .= $addresses[$count]['houseNumber'];
+							break;
+						}
+						$count += 1;
+					}
+					echo "<tr>";
+					echo "<td hidden><input name='hiddenID[]' value=" . $user['id'] . "></td>";
+					echo "<td><input class='changeMember' name='surname[]' id='surname" . $user['id'] . "'type='text' value='" . $user['surname'] . "'  disabled></td>";
+					echo "<td><input class='changeMember' name='name[]' id='name" . $user['id'] . "' type='text' value='" . $user['name'] . "'  disabled></td>";
+					echo "<td><input class='changeMember' name='nameEmail[]' id='email" . $user['id'] . "' type='text' value='" . $user['email'] . "'  disabled></td>";
+					echo "<td><input class='changeMember' name='nameAddress[]' id='address" . $user['id'] . "' type='text' value='" . $stringAddress . "'  disabled></td>";
+					echo "<td><input class='changeMember' name='nameFee[]' id='fee" . $user['id'] . "' type='text' value='" . $user['fee'] . "'  disabled></td>";
+					echo "<td><input class='changeMember' name='joinedAt[]' id='joinedAt" . $user['id'] . "' type='text' value='" . date('Y-m-d', strtotime($user['joinedAt'])) . "'  disabled></td>";
+					echo "<td><input class='changeMember' name='exitAt[]' id='exitAt" . $user['id'] . "' type='text' value='" . $exitDate . "'  disabled></td>";
+					echo "<td class='checkBoxBorder'><input class='checkBox' id='deleteCheckbox" . $user['id'] . "' type='checkbox' name='deleteCheckBox[]' value=" . $user['id'] . "></td>";
+					echo "</tr>";
+					
+				}
+				?>
+			</table>
+		</div>
+	</form>
 </div>
 <script>
-
-	window.onload = getMember();
 	
-	function addMemberLine() {
+	function addMemberToDatabase() {
+		
+		
+		const input = document.querySelectorAll(".addMember");
+			input.forEach(input => input.value = "");
+	}
+	
+	function addMemberFunction() {
 		console.log(1);
 		const element = document.getElementById("cancelMember");
 		const style = window.getComputedStyle(element);
 		
-		if(style.display === "none") {
-			console.log(2);
-			document.getElementById("cancelMember").style.display = "initial";
-			document.getElementById("addMemberLine").style.display = "table-row";
-			document.getElementById("change").style.display = "none";
-			document.getElementById("delete").style.display = "none";
-			
-		}else{
-			
-			
-			console.log(3);
-			const input = document.querySelectorAll(".addMember");
-			input.forEach(input => input.value = "");
-		}
+		document.getElementById("addMemberLine").style.display = "table-row";
+		document.getElementById("cancelMember").style.display = "initial";
+		document.getElementById("addMember").style.display = "table-row";
+		document.getElementById("add").style.display = "none";
+		document.getElementById("change").style.display = "none";
+		document.getElementById("delete").style.display = "none";
+		document.getElementById("form").action = "MitgliederVerwalten/postUser";
 	}
 	
 	function cancel() {
+		document.getElementById("addMember").style.display = "none";
 		document.getElementById("cancelMember").style.display = "none";
 		document.getElementById("addMemberLine").style.display = "none";
 		document.getElementById("change").style.display = "initial";
 		document.getElementById("delete").style.display = "initial";
 		document.getElementById("add").style.display = "initial";
 		document.getElementById("save").style.display = "none";
+		document.getElementById("deleteMember").style.display = "none";
 		
 		var inputs = document.getElementsByClassName('changeMember');
 		for(var i = 0; i < inputs.length; i++) {
 			inputs[i].disabled = true;
 		}
 		
-		document.getElementById("checkBoxBorder").style.border = "none";
-		document.getElementById("checkBoxBorder").style.background = "none";
-		document.getElementById("deleteCheckbox").style.display = "none";
+		var inputs = document.getElementsByClassName('checkBoxBorder');
+		for(var i = 0; i < inputs.length; i++) {
+			inputs[i].style.display = "none";
+		}
 	}
 	
-	function changeMember() {
+	function changeMemberFunction() {
 		
 		document.getElementById("add").style.display = "none";
 		document.getElementById("delete").style.display = "none";
 		document.getElementById("cancelMember").style.display = "initial";
 		document.getElementById("change").style.display = "none";
 		document.getElementById("save").style.display = "initial";
+		document.getElementById("form").action = "MitgliederVerwalten/putUser";
 		
 		var inputs = document.getElementsByClassName('changeMember');
 		for(var i = 0; i < inputs.length; i++) {
@@ -116,40 +143,18 @@
 		
 	}
 	
-	function deleteMember() {
+	function deleteMemberFunction() {
 		
 		document.getElementById("add").style.display = "none";
 		document.getElementById("change").style.display = "none";
+		document.getElementById("delete").style.display = "none";
 		document.getElementById("cancelMember").style.display = "initial";
+		document.getElementById("deleteMember").style.display = "initial";
+		document.getElementById("form").action = "MitgliederVerwalten/deleteUser";
 		
-		document.getElementById("checkBoxBorder").style.border = "1px solid #333";
-		document.getElementById("checkBoxBorder").style.background = "#333";
-		document.getElementById("deleteCheckbox").style.display = "table-row";
-		
-	}
-	
-	function getMember() {
-		
-		//const response = await fetch("");
-		//const daten = await response.json();
-		//const tableBody = document.querySelector('#members tbody');
-		
-		daten.forEach(row => {
-			
-			const tr = document.createElement("tr");
-			
-			tr.innerHTML = `
-				<td><input type="text" value="${row.name}" disabled></td>
-				<td><input type="text" value="${row.surname}" disabled></td>
-				<td><input type="text" value="${row.sports}" disabled></td>
-				<td><input type="text" value="${row.address}" disabled></td>
-				<td><input type="text" value="${row.fee}" disabled></td>
-				<td><input type="text" value="${row.joinedAt}" disabled></td>
-				<td><input type="text" value="${row.exitAt}" disabled></td>
-			`;
-			
-			tableBody.appendChild(tr);
-		})
-		
+		var inputs = document.getElementsByClassName('checkBoxBorder');
+		for(var i = 0; i < inputs.length; i++) {
+			inputs[i].style.display = "block";
+		}
 	}
 </script>
